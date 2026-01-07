663216475d3da8ff19019d8f9b67663d2892ac32
JatinBitla <jatinkoushikbitla@gmail.com>
2026-01-07 16:29:13 +0530

    Import protect middleware in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
+const { protect } = require('./middleware/authMiddleware');
 
 dotenv.config({ path: '../.env' }); // Reaches up to the root.env
 
